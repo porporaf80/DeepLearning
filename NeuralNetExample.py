a49ac124ee33b4210514d95f8a7a089ae1aa12f3
porporaf80 <129082139+porporaf80@users.noreply.github.com>
2024-11-08 18:32:50 +0100

    Create NeuralNetExample.py

---
+++ NeuralNetExample.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+weights = np.around(np.random.uniform(size=6), decimals=2) # initialize the weights
+biases = np.around(np.random.uniform(size=3), decimals=2) # initialize the biases
+
+print(weights)
+print(biases)
+
+x_1 = 0.5 # input 1
+x_2 = 0.85 # input 2
+
+print('x1 is {} and x2 is {}'.format(x_1, x_2))
+
+z_11 = x_1 * weights[0] + x_2 * weights[1] + biases[0]
+
+print('The weighted sum of the inputs at the first node in the hidden layer is {}'.format(z_11))
+
+z_12 = x_1 * weights[2] + x_2 * weights[3] + biases[1]
+print('The weighted sum of the inputs at the second node in the hidden layer is {}'.format(np.around(z_12, decimals=4)))
+
+#Next, assuming a sigmoid activation function, let's compute the activation of the first node,  𝑎1,1
+# , in the hidden layer.
+
+a_11 = 1.0 / (1.0 + np.exp(-z_11))
+
+print('The activation of the first node in the hidden layer is {}'.format(np.around(a_11, decimals=4)))
+
+a_12 = 1.0 / (1.0 + np.exp(-z_12))
+
+print('The activation of the second node in the hidden layer is {}'.format(np.around(a_12, decimals=4)))
+z_2 = a_11 * weights[4] + a_12 * weights[5] + biases[2]
+
+print('The weighted sum of the inputs at the node in the output layer is {}'.format(np.around(z_2, decimals=4)))
+
+a_2 = 1.0 / (1.0 + np.exp(-z_2))
+
+print('The output of the network for x1 = 0.5 and x2 = 0.85 is {}'.format(np.around(a_2, decimals=4)))
+
+
+#Multiple node network
+
+
+n = 2 # number of inputs
+num_hidden_layers = 2 # number of hidden layers
+m = [2, 2] # number of nodes in each hidden layer
+num_nodes_output = 1 # number of nodes in the output layer
+
+import numpy as np # import the Numpy library
+
+def initialize_network(num_inputs, num_hidden_layers, num_nodes_hidden, num_nodes_output):
+    
+    num_nodes_previous = num_inputs # number of nodes in the previous layer
+
+    network = {}
+    
+    # loop through each layer and randomly initialize the weights and biases associated with each layer
+    for layer in range(num_hidden_layers + 1):
+        
+        if layer == num_hidden_layers:
+            layer_name = 'output' # name last layer in the network output
+            num_nodes = num_nodes_output
+        else:
+            layer_name = 'layer_{}'.format(layer + 1) # otherwise give the layer a number
+            num_nodes = num_nodes_hidden[layer] 
+        
+        # initialize weights and bias for each node
+        network[layer_name] = {}
+        for node in range(num_nodes):
+            node_name = 'node_{}'.format(node+1)
+            network[layer_name][node_name] = {
+                'weights': np.around(np.random.uniform(size=num_nodes_previous), decimals=2),
+                'bias': np.around(np.random.uniform(size=1), decimals=2),
+            }
+    
+        num_nodes_previous = num_nodes
+
+    return network # return the network
+
+small_network = initialize_network(5, 3, [3, 2, 3], 1)
+
+print('small_network:',small_network)
+
+#The weighted sum at each node
+
+def compute_weighted_sum(inputs, weights, bias):
+    return np.sum(inputs * weights) + bias
+
+# Generiamo 12 numeri a caso da dar e in pasto alla rete neurale
+from random import seed
+import numpy as np
+
+np.random.seed(12)
+inputs = np.around(np.random.uniform(size=5), decimals=2)
+
+print('The inputs to the network are {}'.format(inputs))
+
+#calcoliamo la somma pesata al primo nodo del primo layer
+node_weights = small_network['layer_1']['node_1']['weights']
+node_bias = small_network['layer_1']['node_1']['bias']
+
+weighted_sum = compute_weighted_sum(inputs, node_weights, node_bias)
+print('The weighted sum at the first node in the hidden layer is {}'.format(np.around(weighted_sum[0], decimals=4)))
+
+#Funzione node activation
+def node_activation(weighted_sum):
+    return 1.0 / (1.0 + np.exp(-1 * weighted_sum))
+
+node_output  = node_activation(compute_weighted_sum(inputs, node_weights, node_bias))
+print('The output of the first node in the hidden layer is {}'.format(np.around(node_output[0], decimals=4)))
+
+
+print('Node activation ',node_output)
+
+#Funzione forward propagation 
+
+def forward_propagate(network, inputs):
+    
+    layer_inputs = list(inputs) # start with the input layer as the input to the first hidden layer
+    
+    for layer in network:
+        
+        layer_data = network[layer]
+        
+        layer_outputs = [] 
+        for layer_node in layer_data:
+        
+            node_data = layer_data[layer_node]
+        
+            # compute the weighted sum and the output of each node at the same time 
+            node_output = node_activation(compute_weighted_sum(layer_inputs, node_data['weights'], node_data['bias']))
+            layer_outputs.append(np.around(node_output[0], decimals=4))
+            
+        if layer != 'output':
+            print('The outputs of the nodes in hidden layer number {} is {}'.format(layer.split('_')[1], layer_outputs))
+    
+        layer_inputs = layer_outputs # set the output of this layer to be the input to next layer
+
+    network_predictions = layer_outputs
+    return network_predictions
+
+
+output_layer = forward_propagate(small_network,inputs)
+
+print(output_layer)
+
+my_network = initialize_network(5, 3, [2, 3, 2], 3)
+
+inputs = np.around(np.random.uniform(size=5), decimals=2)
+
+predictions = forward_propagate(my_network, inputs)
+print('The predicted values by the network for the given input are {}'.format(predictions))
+
+
+my_network2 =initialize_network(6,6,[3,6,7,8,8,5],1)
+inputs = np.around(np.random.uniform(size=6),decimals=3)
+predictions = forward_propagate(my_network2, inputs)
+print('The predicted values by the network for the given input are {}'.format(predictions))   
