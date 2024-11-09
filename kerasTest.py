e049c48fb4bcbad4aae4718125d65c24db6d66a5
porporaf80 <porpora.f@pg.com>
2024-11-09 14:06:16 +0100

    Keras

--- kerasTest.py
+++ kerasTest.py
@@ -1,5 +1,17 @@
-import kearas
-from keras.models import Sequential
-from keras.layers import Dense
+# All Libraries required for this lab are listed below. The libraries pre-installed on Skills Network Labs are commented. 
+# If you run this notebook on a different environment, e.g. your desktop, you may need to uncomment and install certain libraries.
 
-model =Sequential()
+#!pip install numpy==1.21.4
+#!pip install pandas==1.3.4
+#!pip install keras==2.1.6
+
+import pandas as pd
+import numpy as np
+
+import warnings
+warnings.simplefilter('ignore', FutureWarning)
+
+concrete_data = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DL0101EN/labs/data/concrete_data.csv')
+concrete_data.head()
+#print (concrete_data)
+print(concrete_data.shape)
