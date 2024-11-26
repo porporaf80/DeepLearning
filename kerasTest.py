735761fe3114c9eba7127bc1f7ea968b05c5fefa
porporaf80 <porpora.f@pg.com>
2024-11-26 10:05:00 +0100

    test

--- kerasTest.py
+++ kerasTest.py
@@ -4,14 +4,14 @@
 #!pip install numpy==1.21.4
 #!pip install pandas==1.3.4
 #!pip install keras==2.1.6
-
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 
 import warnings
 warnings.simplefilter('ignore', FutureWarning)
 
-concrete_data = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DL0101EN/labs/data/concrete_data.csv')
+concrete_data = pd.read_csv('concrete_data.csv')
 concrete_data.head()
 #print (concrete_data)
 print(concrete_data.shape)
@@ -36,6 +36,24 @@ predictors_norm.head()
 
 n_cols = predictors_norm.shape[1] # number of predictors
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+
+
+
+# define regression model
+def regression_model():
+    # create model
+    model = tf.keras.Sequential()
+    
+    model.add(10, activation='relu', input_shape=(n_cols,))
+    model.add(50, activation='relu')
+    model.add(1)
+    
+    # compile model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+
+model = regression_model()
+
+# fit the model
+model.fit(predictors_norm, target, validation_split=0.3, epochs=100, verbose=2)
